3567746f0175f2ebdb206140e1c4606b7f6292ee
peterneema3 <PeterNeema@PC-G84784S4>
2026-03-31 09:56:05 +0100

    Added inspector and viewer log ins

--- oop-s2-2-mvc-77366/Program.cs
+++ oop-s2-2-mvc-77366/Program.cs
@@ -48,7 +48,7 @@ builder.Host.UseSerilog();
 
 var app = builder.Build();
 
-// Seed database + roles + admin user
+// Seed database + roles + default users
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -101,6 +101,64 @@ using (var scope = app.Services.CreateScope())
     if (!await userManager.IsInRoleAsync(adminUser, "Admin"))
     {
         await userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+
+    // Create inspector user
+    string inspectorEmail = "neemapeter537@gmail.com";
+    string inspectorPassword = "Fawe20212022!";
+
+    var inspectorUser = await userManager.FindByEmailAsync(inspectorEmail);
+
+    if (inspectorUser == null)
+    {
+        inspectorUser = new IdentityUser
+        {
+            UserName = inspectorEmail,
+            Email = inspectorEmail,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(inspectorUser, inspectorPassword);
+
+        if (!result.Succeeded)
+        {
+            throw new Exception("Inspector user could not be created.");
+        }
+    }
+
+    // Ensure inspector is always in Inspector role
+    if (!await userManager.IsInRoleAsync(inspectorUser, "Inspector"))
+    {
+        await userManager.AddToRoleAsync(inspectorUser, "Inspector");
+    }
+
+    // Create viewer user
+    string viewerEmail = "neemabusiness3@gmail.com";
+    string viewerPassword = "Fawe20222023!";
+
+    var viewerUser = await userManager.FindByEmailAsync(viewerEmail);
+
+    if (viewerUser == null)
+    {
+        viewerUser = new IdentityUser
+        {
+            UserName = viewerEmail,
+            Email = viewerEmail,
+            EmailConfirmed = true
+        };
+
+        var result = await userManager.CreateAsync(viewerUser, viewerPassword);
+
+        if (!result.Succeeded)
+        {
+            throw new Exception("Viewer user could not be created.");
+        }
+    }
+
+    // Ensure viewer is always in Viewer role
+    if (!await userManager.IsInRoleAsync(viewerUser, "Viewer"))
+    {
+        await userManager.AddToRoleAsync(viewerUser, "Viewer");
     }
 }
 
